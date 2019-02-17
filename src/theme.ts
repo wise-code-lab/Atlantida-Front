722f1e782ad891c285fa39fb3aab09d3d747ef0a
MizovOO <MizovOO@ua.fm>
2019-02-17 16:45:42 +0200

    Commented gradien color from buttons

--- src/theme.ts
+++ src/theme.ts
@@ -13,12 +13,12 @@ export const theme = createMuiTheme({
       // Name of the rule
       text: {
         // Some CSS
-        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-        borderRadius: 3,
-        border: 0,
-        color: 'white',
-        height: 36,
-        padding: '0 30px'
+        // background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        // borderRadius: 3,
+        // border: 0,
+        // color: 'white',
+        // height: 36,
+        // padding: '0 30px'
       },
     },
   },
